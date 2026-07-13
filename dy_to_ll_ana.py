e5170f3b9f046c7136b369b954230695e0b0909e
Abanti Ranadhir Sahasransu <a.r.sahasransu@cern.ch>
2026-07-13 10:18:48 +0200

    Add Ntupliser for ML, pt cuts on el and puppi cands. Keep legacy puppi iso definition unchanged.

--- dy_to_ll_ana.py
+++ dy_to_ll_ana.py
@@ -34,10 +34,14 @@ def dy_to_ll_ana_main(ana_man: anut.SampleRDFManager) -> anut.SampleRDFManager:
     # rdf_g.add_hists_singlecollection(df_genstudy0, histograms, f'{sufGen}_GENST0')    
     # ##########################################################
 
-    df = rdf_g.define_newcollection(df, sufGen, f'{sufGen}_prompt==2 && abs({sufGen}_eta)<2.5', 'DYP')
+    df = rdf_g.define_newcollection(df, sufGen, f'{sufGen}_prompt==2 && abs({sufGen}_eta)<=2.5', 'DYP')
+    df = rdf_g.define_newcollection(df, sufEl, f'{sufEl}_pt >= 5.0', 'Pt5')
+    sufElPt5 = sufEl+'_Pt5'
+    df = rdf_g.define_newcollection(df, sufPu, f'{sufPu}_pt >= 1.0', 'Pt1')
+    sufPuPt1 = sufPu+'_Pt1'
     rdf_g.add_hists_singlecollection(df, histograms, f'{sufGen}_DYP')
 
-    dfGenP = df.Filter(f'{sufGen}_DYP_n > 0 && {sufEl}_n > 0', 'genDYP')
+    dfGenP = df.Filter(f'{sufGen}_DYP_n > 0 && {sufElPt5}_n > 0', 'genDYP')
     ana_man.add_dataframe(key='genDYP', df=dfGenP)
     ut.create_rdf_checkpint(df, dfGenP, "Applying selection: >0 Gen electron with prompt "
                                         "status 2 and |eta| < 2.5... \n>0 reconstructed TkEl...")
@@ -45,66 +49,66 @@ def dy_to_ll_ana_main(ana_man: anut.SampleRDFManager) -> anut.SampleRDFManager:
 
     # # STEP 1_2_0: Enable for plots in "Gen selection" section
     # ##########################################################
-    # rdf_g.add_hists_singlecollection(dfGenP, histograms, sufEl)
+    # rdf_g.add_hists_singlecollection(dfGenP, histograms, sufElPt5)
     # add_puppicands_by_pdg(dfGenP, histograms, '')
     # ##########################################################
 
     # SPLIT ETA REGIONS BASED ON TKEL
-    dfGenP = rdf_g.define_newcollection(dfGenP, sufEl, f'abs({sufEl}_eta) <= 1.5', 'EB')
-    dfGenP = rdf_g.define_newcollection(dfGenP, sufEl, f'abs({sufEl}_eta) > 1.5 && abs({sufEl}_eta) <= 2.5', 'EE')
+    dfGenP = rdf_g.define_newcollection(dfGenP, sufElPt5, f'abs({sufElPt5}_eta) <= 1.47', 'EB')
+    dfGenP = rdf_g.define_newcollection(dfGenP, sufElPt5, f'abs({sufElPt5}_eta) > 1.47 && abs({sufElPt5}_eta) <= 2.5', 'EE')
 
     gen_dRcuts = {'EB': 0.04, 'EE': 0.05}
 
     for ERegion in ['EB', 'EE']:
     # for ERegion in ['EB']:
         sufGenDYP = f'{sufGen}_DYP'
-        sufElER = f'{sufEl}_{ERegion}'
-        dfGenER = dfGenP.Filter(f'{sufElER}_n > 0', f'DYP{ERegion}')
+        sufElPt5ER = f'{sufElPt5}_{ERegion}'
+        dfGenER = dfGenP.Filter(f'{sufElPt5ER}_n > 0', f'DYP{ERegion}')
         ut.create_rdf_checkpint(dfGenP, dfGenER, f"Applying selection: > 0 TkEl in region {ERegion}...")
 
         # # STEP 2_0_0 AND 2_1_0: GEN MATCH BLOCK - PRE GEN MATCH
         # #########################################################
-        # dfGenER = anut.angdiff_hists(dfGenER, sufGenDYP, sufElER)
-        # rdf_g.add_hists_multiplecolls(dfGenER, histograms, [f'{sufElER}', f'{sufGenDYP}_{sufElER}'])
-        # anut.add_genmatching_efficiency_with_dRcut(histograms, f'DYP{ERegion}_{sufGenDYP}_{sufElER}')
+        # dfGenER = anut.angdiff_hists(dfGenER, sufGenDYP, sufElPt5ER)
+        # rdf_g.add_hists_multiplecolls(dfGenER, histograms, [f'{sufElPt5ER}', f'{sufGenDYP}_{sufElPt5ER}'])
+        # anut.add_genmatching_efficiency_with_dRcut(histograms, f'DYP{ERegion}_{sufGenDYP}_{sufElPt5ER}')
         # ##########################################################
 
         # GEN MATCH
-        dfGenER = anut.do_gen_match(dfGenER, sufGenDYP, sufElER, gen_dRcuts[ERegion])
+        dfGenER = anut.do_gen_match(dfGenER, sufGenDYP, sufElPt5ER, gen_dRcuts[ERegion])
         dfGenER = rdf_g.define_newcollection(dfGenER, sufGenDYP, f'{sufGenDYP}_recoidx != -1', 'MCH')
-        dfGenER = rdf_g.define_newcollection(dfGenER, sufElER, f'{sufElER}_genidx != -1', 'MCH')
+        dfGenER = rdf_g.define_newcollection(dfGenER, sufElPt5ER, f'{sufElPt5ER}_genidx != -1', 'MCH')
         
         sufGenMch = f'{sufGenDYP}_MCH'
-        sufElMch = f'{sufElER}_MCH'
+        sufElPt5Mch = f'{sufElPt5ER}_MCH'
 
         # # STEP 2_0_0: GEN MATCH BLOCK - POST GEN MATCH
         # #########################################################
-        # dfGenER = anut.angdiff_hists(dfGenER, sufGenMch, sufElMch)
-        # rdf_g.add_hists_singlecollection(dfGenER, histograms, f'{sufGenMch}_{sufElMch}')
+        # dfGenER = anut.angdiff_hists(dfGenER, sufGenMch, sufElPt5Mch)
+        # rdf_g.add_hists_singlecollection(dfGenER, histograms, f'{sufGenMch}_{sufElPt5Mch}')
         # ##########################################################
 
         # STEP 2_1_0 AND 3_0_0: For comparing all TkEl to gen-matched TkEl
         #########################################################
-        rdf_g.add_hists_singlecollection(dfGenER, histograms, sufElMch)
+        rdf_g.add_hists_singlecollection(dfGenER, histograms, sufElPt5Mch)
         ana_man.add_dataframe(key=f'DYP{ERegion}', df=dfGenER)
         #########################################################
 
         # Filter for atleast one gen-match TkEl in the defined eta region
-        dfGenMER = dfGenER.Filter(f'{sufElMch}_n > 0', f'GM')
+        dfGenMER = dfGenER.Filter(f'{sufElPt5Mch}_n > 0', f'GM')
         ut.create_rdf_checkpint(dfGenER, dfGenMER, f"Applying selection: > 0 Gen-matched TkEl in region {ERegion}")
 
         # STEP 3_0_0: Add charged contribution to iso calc for gen-matched TkEl
         #########################################################
-        dfGenMER = reiso.recalculate_puppi_iso(dfGenMER, sufElMch, sufPu)
-        # dfGenMER = reiso.recalculate_puppi_iso(dfGenMER, sufElMch, sufPu, drminlist=[0.01], drmax=0.4, ptmin=2, dzmax=1.0)
+        dfGenMER = reiso.recalculate_puppi_iso(dfGenMER, sufElPt5Mch, sufPu)
+        # dfGenMER = reiso.recalculate_puppi_iso(dfGenMER, sufElPt5Mch, sufPuPt1, drminlist=[0.01], drmax=0.4, ptmin=2, dzmax=1.0)
         # dfGenMER.Describe().Print()
         ana_man.add_dataframe(key=f'DYPM{ERegion}', df=dfGenMER)
-        rdf_g.add_hists_multiplecolls(dfGenMER, histograms, [sufElMch,
-                                        sufElMch+r'_reisotot2026:dRmin\d_\d{1,2}',
-                                        sufElMch+r'_reisotot:dRmin\d_\d{1,2}',
-                                        sufElMch+r'_reisooth:dRmin\d_\d{1,2}',
-                                        sufElMch+r'_reisochg:dRmin\d_\d{1,2}',
-                                        sufElMch+r'_reisonut:dRmin\d_\d{1,2}'])
+        rdf_g.add_hists_multiplecolls(dfGenMER, histograms, [sufElPt5Mch,
+                                        sufElPt5Mch+r'_reisotot2026:dRmin\d_\d{1,2}',
+                                        sufElPt5Mch+r'_reisotot:dRmin\d_\d{1,2}',
+                                        sufElPt5Mch+r'_reisooth:dRmin\d_\d{1,2}',
+                                        sufElPt5Mch+r'_reisochg:dRmin\d_\d{1,2}',
+                                        sufElPt5Mch+r'_reisonut:dRmin\d_\d{1,2}'])
         #########################################################
 
     ana_man.add_histograms(histograms)
@@ -112,43 +116,43 @@ def dy_to_ll_ana_main(ana_man: anut.SampleRDFManager) -> anut.SampleRDFManager:
 
         # STEP 4_0_0: Compare in pT separate bins
         #########################################################
-        # dfGenMER = rdf_g.define_newcollection(dfGenMER, sufElMch, f"{sufElMch}_pt > 50", "Pt50")
-        # dfGenMER = rdf_g.define_newcollection(dfGenMER, sufElMch, f"{sufElMch}_pt > 20 && {sufElMch}_pt <= 50", "20Pt50")
-        # rdf_g.add_hists_multiplecolls(dfGenMER, histograms, [f"{sufElMch}_Pt50", f"{sufElMch}_20Pt50"])
+        # dfGenMER = rdf_g.define_newcollection(dfGenMER, sufElPt5Mch, f"{sufElPt5Mch}_pt > 50", "Pt50")
+        # dfGenMER = rdf_g.define_newcollection(dfGenMER, sufElPt5Mch, f"{sufElPt5Mch}_pt > 20 && {sufElPt5Mch}_pt <= 50", "20Pt50")
+        # rdf_g.add_hists_multiplecolls(dfGenMER, histograms, [f"{sufElPt5Mch}_Pt50", f"{sufElPt5Mch}_20Pt50"])
         #########################################################
 
         # dfGenMER.Describe().Print()
 
     # # # Checked for pT sorting of the TkEl collection
-    # dfGenER = dfGenER.Define(f'{sufElMch}_pt_sorted', f'checksorting<float>({sufElMch}_pt, true)')
-    # histograms.append( dfGenER.Histo1D((f'{sufElMch}_pt_sorted', 'pt_sort', 6, -2, 4), f'{sufElMch}_pt_sorted') )
-    # cp_pt_sort = anut.check_histogram_for_value(histograms, f'{sufElMch}_pt_sorted', bin=3) # bin 3 = value 0
+    # dfGenER = dfGenER.Define(f'{sufElPt5Mch}_pt_sorted', f'checksorting<float>({sufElPt5Mch}_pt, true)')
+    # histograms.append( dfGenER.Histo1D((f'{sufElPt5Mch}_pt_sorted', 'pt_sort', 6, -2, 4), f'{sufElPt5Mch}_pt_sorted') )
+    # cp_pt_sort = anut.check_histogram_for_value(histograms, f'{sufElPt5Mch}_pt_sorted', bin=3) # bin 3 = value 0
     # if cp_pt_sort:
     #     raise RuntimeError("The Track Electrons pT's are not sorted from highest to lowest")
 
     # For each TkEl, observe the feature of PuppiCandidates around the TkEl in annular dR window
-    # add_puppicands_by_pdg(dfGenER, histograms, '', tkelobj=sufElMch)
-    # dfGenER = anut.make_puppi_by_angdiff_from_tkel(dfGenER, sufElMch, histograms)
+    # add_puppicands_by_pdg(dfGenER, histograms, '', tkelobj=sufElPt5Mch)
+    # dfGenER = anut.make_puppi_by_angdiff_from_tkel(dfGenER, sufElPt5Mch, histograms)
 
     # Separate the lead and sub-lead gen matched TkEl
-    dfgEBel1 = dfgenEB.Filter(f'{sufEl}_MCH_n >= 1', 'genDYEBTkEl1')
-    dfgEBel1 = rdf_g.define_newcollection(dfgEBel1, f'{sufEl}_MCH', '0', 'El1')
-    rdf_g.add_hists_singlecollection(dfgEBel1, histograms, f'{sufEl}_MCH_El1')
+    dfgEBel1 = dfgenEB.Filter(f'{sufElPt5}_MCH_n >= 1', 'genDYEBTkEl1')
+    dfgEBel1 = rdf_g.define_newcollection(dfgEBel1, f'{sufElPt5}_MCH', '0', 'El1')
+    rdf_g.add_hists_singlecollection(dfgEBel1, histograms, f'{sufElPt5}_MCH_El1')
     # Puppi isolation and components by pdgId
-    dfgEBel1 = reiso.recalcpuppiiso_comps_oneel(dfgEBel1, f'{sufEl}_MCH_El1', sufPu)
-    rdf_g.add_hists_singlecollection(dfgEBel1, histograms, f'{sufEl}_MCH_El1_Re',
+    dfgEBel1 = reiso.recalcpuppiiso_comps_oneel(dfgEBel1, f'{sufElPt5}_MCH_El1', sufPuPt1)
+    rdf_g.add_hists_singlecollection(dfgEBel1, histograms, f'{sufElPt5}_MCH_El1_Re',
                                      'dRmin\\d_\\d{1,2}_dRmax\\d_\\d{1,2}_[a-z0-9]+')
 
-    # dfgEBel2 = dfgenEB.Filter(f'{sufEl}_MCH_n >= 2', 'genDYEBTkEl2')
-    # dfgEBel2 = rdf_g.define_newcollection(dfgEBel2, f'{sufEl}_MCH', '1', 'El2')
-    # rdf_g.add_hists_singlecollection(dfgEBel2, histograms, f'{sufEl}_MCH_El2')
+    # dfgEBel2 = dfgenEB.Filter(f'{sufElPt5}_MCH_n >= 2', 'genDYEBTkEl2')
+    # dfgEBel2 = rdf_g.define_newcollection(dfgEBel2, f'{sufElPt5}_MCH', '1', 'El2')
+    # rdf_g.add_hists_singlecollection(dfgEBel2, histograms, f'{sufElPt5}_MCH_El2')
 
     # Calculate the invariant mass
-    # dfgEBel2 = dfgEBel2.Define(f'{sufEl}_MCH_m', f"ROOT::VecOps::RVec<float>({sufEl}_MCH_pt.size(), 0.000511)")
-    # dfgEBel2 = dfgEBel2.Define(f'{sufEl}_MCH_Mee', f'ROOT::VecOps::InvariantMass<float>({sufEl}_MCH_pt,\
-    # {sufEl}_MCH_eta, {sufEl}_MCH_phi, {sufEl}_MCH_m)')
-    # histograms.append( dfgEBel2.Histo1D((f'genDYEBTkEl2_{sufEl}_MCH_Mee', 'mass [GeV]', 201, -1, 200),\
-    # f'{sufEl}_MCH_Mee') )
+    # dfgEBel2 = dfgEBel2.Define(f'{sufElPt5}_MCH_m', f"ROOT::VecOps::RVec<float>({sufElPt5}_MCH_pt.size(), 0.000511)")
+    # dfgEBel2 = dfgEBel2.Define(f'{sufElPt5}_MCH_Mee', f'ROOT::VecOps::InvariantMass<float>({sufElPt5}_MCH_pt,\
+    # {sufElPt5}_MCH_eta, {sufElPt5}_MCH_phi, {sufElPt5}_MCH_m)')
+    # histograms.append( dfgEBel2.Histo1D((f'genDYEBTkEl2_{sufElPt5}_MCH_Mee', 'mass [GeV]', 201, -1, 200),\
+    # f'{sufElPt5}_MCH_Mee') )
 
     # dfgenEB_tkel2.Describe().Print()
 
--- pypkg/post_analysis_persample.py
+++ pypkg/post_analysis_persample.py
@@ -11,28 +11,32 @@ def post_analysis_persample(anamanager: SampleRDFManager):
 
         # STEP 3_0_0, X_X_X: For ROC curves
         DYPEB_df = anamanager.get_dataframe('DYPMEB')
-        cols = ['TkEleL2_EB_MCH_pt', 'TkEleL2_EB_MCH_tkIso', 'TkEleL2_EB_MCH_absTkIso',
-                'TkEleL2_EB_MCH_puppiIso',
-                'TkEleL2_EB_MCH_reisotot_dRmin0_03_puppiIso',
-                'TkEleL2_EB_MCH_reisochg_dRmin0_03_puppiIso',
-                # 'TkEleL2_EB_MCH_reisonut_dRmin0_03_puppiIso',
-                # 'TkEleL2_EB_MCH_reisotot_dRmin0_01_puppiIso',
-                # 'TkEleL2_EB_MCH_reisochg_dRmin0_01_puppiIso',
-                # 'TkEleL2_EB_MCH_reisonut_dRmin0_01_puppiIso',
+        cols = ['TkEleL2_Pt5_EB_MCH_pt', 'TkEleL2_Pt5_EB_MCH_tkIso', 'TkEleL2_Pt5_EB_MCH_absTkIso',
+                'TkEleL2_Pt5_EB_MCH_puppiIso',
+                'TkEleL2_Pt5_EB_MCH_reisotot_dRmin0_03_puppiIso',
+                'TkEleL2_Pt5_EB_MCH_reisochg_dRmin0_03_puppiIso',
+                # 'TkEleL2_Pt5_EB_MCH_reisonut_dRmin0_03_puppiIso',
+                # 'TkEleL2_Pt5_EB_MCH_reisotot_dRmin0_01_puppiIso',
+                # 'TkEleL2_Pt5_EB_MCH_reisochg_dRmin0_01_puppiIso',
+                # 'TkEleL2_Pt5_EB_MCH_reisonut_dRmin0_01_puppiIso',
             ]
+        cols += add_ntuples_for_aialgos_dy_eb()
+        cols += add_ntuples_for_aialgos_puppi()
         rdf_g.save_rdf_snapshot(DYPEB_df, cols, f"{sname}_EB", recreate=True)
 
         # STEP 3_0_0, X_X_X: For ROC curves
         DYPEE_df = anamanager.get_dataframe('DYPMEE')
-        cols = ['TkEleL2_EE_MCH_pt', 'TkEleL2_EE_MCH_tkIso', 'TkEleL2_EE_MCH_absTkIso',
-                'TkEleL2_EE_MCH_puppiIso',
-                'TkEleL2_EE_MCH_reisotot_dRmin0_03_puppiIso',
-                'TkEleL2_EE_MCH_reisochg_dRmin0_03_puppiIso',
-                # 'TkEleL2_EE_MCH_reisonut_dRmin0_03_puppiIso',
-                # 'TkEleL2_EE_MCH_reisotot_dRmin0_01_puppiIso',
-                # 'TkEleL2_EE_MCH_reisochg_dRmin0_01_puppiIso',
-                # 'TkEleL2_EE_MCH_reisonut_dRmin0_01_puppiIso',
+        cols = ['TkEleL2_Pt5_EE_MCH_pt', 'TkEleL2_Pt5_EE_MCH_tkIso', 'TkEleL2_Pt5_EE_MCH_absTkIso',
+                'TkEleL2_Pt5_EE_MCH_puppiIso',
+                'TkEleL2_Pt5_EE_MCH_reisotot_dRmin0_03_puppiIso',
+                'TkEleL2_Pt5_EE_MCH_reisochg_dRmin0_03_puppiIso',
+                # 'TkEleL2_Pt5_EE_MCH_reisonut_dRmin0_03_puppiIso',
+                # 'TkEleL2_Pt5_EE_MCH_reisotot_dRmin0_01_puppiIso',
+                # 'TkEleL2_Pt5_EE_MCH_reisochg_dRmin0_01_puppiIso',
+                # 'TkEleL2_Pt5_EE_MCH_reisonut_dRmin0_01_puppiIso',
             ]  
+        cols += add_ntuples_for_aialgos_dy_ee()
+        cols += add_ntuples_for_aialgos_puppi()
         rdf_g.save_rdf_snapshot(DYPEE_df, cols, f"{sname}_EE", recreate=True)
 
     if sname == "MinBias":
@@ -49,6 +53,8 @@ def post_analysis_persample(anamanager: SampleRDFManager):
                 # 'TkEleL2_Pt5_EB_reisochg_dRmin0_01_puppiIso',
                 # 'TkEleL2_Pt5_EB_reisonut_dRmin0_01_puppiIso',
             ]
+        cols += add_ntuples_for_aialgos_minbias_eb()
+        cols += add_ntuples_for_aialgos_puppi()
         rdf_g.save_rdf_snapshot(EB_df, cols, f"{sname}_EB", recreate=True)
 
         # STEP 3_0_0, X_X_X: For ROC curves
@@ -62,4 +68,56 @@ def post_analysis_persample(anamanager: SampleRDFManager):
                 # 'TkEleL2_Pt5_EE_reisochg_dRmin0_01_puppiIso',
                 # 'TkEleL2_Pt5_EE_reisonut_dRmin0_01_puppiIso',
         ]
+        cols += add_ntuples_for_aialgos_minbias_ee()
+        cols += add_ntuples_for_aialgos_puppi()
+
         rdf_g.save_rdf_snapshot(EE_df, cols, f"{sname}_EE", recreate=True)
+
+
+def add_ntuples_for_aialgos_dy_eb():
+
+    cols = ['TkEleL2_Pt5_EB_MCH_eta', 'TkEleL2_Pt5_EB_MCH_phi',
+            'TkEleL2_Pt5_EB_MCH_caloEta', 'TkEleL2_Pt5_EB_MCH_caloPhi',
+            'TkEleL2_Pt5_EB_MCH_tkPt', 'TkEleL2_Pt5_EB_MCH_tkEta', 'TkEleL2_Pt5_EB_MCH_tkPhi',
+            'TkEleL2_Pt5_EB_MCH_charge', 'TkEleL2_Pt5_EB_MCH_hwQual', 'TkEleL2_Pt5_EB_MCH_vz']
+
+    return cols
+
+
+def add_ntuples_for_aialgos_dy_ee():
+
+    cols = ['TkEleL2_Pt5_EE_MCH_eta', 'TkEleL2_Pt5_EE_MCH_phi',
+            'TkEleL2_Pt5_EE_MCH_caloEta', 'TkEleL2_Pt5_EE_MCH_caloPhi',
+            'TkEleL2_Pt5_EE_MCH_tkPt', 'TkEleL2_Pt5_EE_MCH_tkEta', 'TkEleL2_Pt5_EE_MCH_tkPhi',
+            'TkEleL2_Pt5_EE_MCH_charge', 'TkEleL2_Pt5_EE_MCH_hwQual', 'TkEleL2_Pt5_EE_MCH_vz']
+
+    return cols
+
+
+def add_ntuples_for_aialgos_minbias_eb():
+
+    cols = ['TkEleL2_Pt5_EB_eta', 'TkEleL2_Pt5_EB_phi',
+            'TkEleL2_Pt5_EB_caloEta', 'TkEleL2_Pt5_EB_caloPhi',
+            'TkEleL2_Pt5_EB_tkPt', 'TkEleL2_Pt5_EB_tkEta', 'TkEleL2_Pt5_EB_tkPhi',
+            'TkEleL2_Pt5_EB_charge', 'TkEleL2_Pt5_EB_hwQual', 'TkEleL2_Pt5_EB_vz']
+    
+    return cols
+
+
+def add_ntuples_for_aialgos_minbias_ee():
+
+    cols = ['TkEleL2_Pt5_EE_eta', 'TkEleL2_Pt5_EE_phi',
+            'TkEleL2_Pt5_EE_caloEta', 'TkEleL2_Pt5_EE_caloPhi',
+            'TkEleL2_Pt5_EE_tkPt', 'TkEleL2_Pt5_EE_tkEta', 'TkEleL2_Pt5_EE_tkPhi',
+            'TkEleL2_Pt5_EE_charge', 'TkEleL2_Pt5_EE_hwQual', 'TkEleL2_Pt5_EE_vz']
+
+    return cols
+
+def add_ntuples_for_aialgos_puppi():
+
+    cols = ['L1PuppiCands_Pt1_pt', 'L1PuppiCands_Pt1_eta', 'L1PuppiCands_Pt1_phi',
+            'L1PuppiCands_Pt1_mass', 'L1PuppiCands_Pt1_charge', 'L1PuppiCands_Pt1_dxy',
+            'L1PuppiCands_Pt1_hwDxy', 'L1PuppiCands_Pt1_hwTkQuality', 'L1PuppiCands_Pt1_pdgId',
+            'L1PuppiCands_Pt1_puppiWeight', 'L1PuppiCands_Pt1_z0']
+
+    return cols
